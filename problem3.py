ef3f1971980b7af96b513526271c7d5151ba261d
Malakabdullah2010 <abdullahmalak470@gmail.com>
2025-10-23 17:42:41 -0500

    Implement is_even function in problem3.py
    Add function to determine if a number is even or odd

--- problem3.py
+++ problem3.py
@@ -1,1 +1,11 @@
-
+def is_even (number):
+  half = number / 2
+    if half == int(half):
+        return "even"
+    else:
+        return "odd"
+    
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    print(f"{num} is {is_even(num)}")
+    
